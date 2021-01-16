094b578009052213dbef6b8c377f5496ea2fb3fa
Ruan <ruangbarros@gmail.com>
2021-01-16 00:16:22 -0300

    Creating model Movie

---
+++ MoviesCRUDRazor/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MoviesCRUDRazor.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate{ get; set; }
+
+        public int Gender { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
