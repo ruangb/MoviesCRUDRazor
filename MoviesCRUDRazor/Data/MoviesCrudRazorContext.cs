fc8efe10559c178c6c024e9d991492b8b2a77157
Ruan <ruangbarros@gmail.com>
2021-01-16 22:15:23 -0300

    Configuring DataContext and Startup

---
+++ MoviesCRUDRazor/Data/MoviesCrudRazorContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesCRUDRazor.Models;
+
+namespace MoviesCRUDRazor.Data
+{
+    public class MoviesCrudRazorContext : DbContext
+    {
+        public MoviesCrudRazorContext(DbContextOptions<MoviesCrudRazorContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
